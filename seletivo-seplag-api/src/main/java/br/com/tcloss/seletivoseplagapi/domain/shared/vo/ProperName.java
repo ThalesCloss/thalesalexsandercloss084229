d56f882e1cf517b523c6ae0244703915389dfbb3
tcloss <thales.a.closs@gmail.com>
2026-01-25 13:22:27 -0400

    feat(domain): value object compartilhado para representar nomes proprios

---
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/domain/shared/vo/ProperName.java
@@ -0,0 +1,23 @@
+package br.com.tcloss.seletivoseplagapi.domain.shared.vo;
+
+import br.com.tcloss.seletivoseplagapi.domain.shared.ValueObject;
+
+public record ProperName(
+    String name
+) implements ValueObject{
+    public ProperName {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Nome não pode ser vazio");
+        }
+        
+        if (name.length() < 2) {
+             throw new IllegalArgumentException("Nome deve ter pelo menos 2 caracteres");
+        }
+        name = name.trim();
+    }
+
+    @Override
+    public final String toString() {
+        return name;
+    }
+}
