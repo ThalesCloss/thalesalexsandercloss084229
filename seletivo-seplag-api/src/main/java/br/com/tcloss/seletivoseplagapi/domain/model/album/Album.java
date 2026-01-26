964f6dd20b33fcb3fe408db599e340f10b02b577
tcloss <thales.a.closs@gmail.com>
2026-01-25 21:42:49 -0400

    feat(domain): modelagem do album

---
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/domain/model/album/Album.java
@@ -0,0 +1,67 @@
+package br.com.tcloss.seletivoseplagapi.domain.model.album;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import br.com.tcloss.seletivoseplagapi.domain.shared.AggregateRoot;
+import br.com.tcloss.seletivoseplagapi.domain.shared.validation.Notification;
+import br.com.tcloss.seletivoseplagapi.domain.shared.vo.ProperName;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Album extends AggregateRoot<UUID> {
+
+    private UUID id;
+
+    private ProperName title;
+
+    private UUID artistProfileId;
+
+    private UUID artistProfileLineupId;
+
+    private LocalDate releaseDate;
+
+    private List<Track> tracks;
+
+    public void addTrack(Track newTrack) {
+        this.tracks.add(newTrack);
+    }
+
+    public void removeTrack(UUID trackId) {
+        boolean removed = this.tracks.removeIf(track -> track.getId().equals(trackId));
+
+        if (!removed) {
+            throw new IllegalArgumentException("Faixa não encontrada no álbum.");
+        }
+    }
+
+    public static Album createNew(
+            String title,
+            UUID artistProfileId,
+            UUID artistProfileLineupId,
+            LocalDate releaseDate,
+            List<Track> tracks) {
+        final var notification = Notification.create();
+        final var properTitle = notification.tryExecute(() -> new ProperName(title));
+        notification.validate(artistProfileId != null && !artistProfileId.toString().isBlank(),
+                "O ID do perfil do artista é obrigatório.");
+        notification.validate(artistProfileLineupId != null && !artistProfileLineupId.toString().isBlank(),
+                "O ID da formação do perfil do artista é obrigatório.");
+        notification.validate(tracks != null && !tracks.isEmpty(), "O album deve conter ao menos uma faixa.");
+        notification.throwIfHasErrors();
+        return new Album(
+                UUID.randomUUID(),
+                properTitle,
+                artistProfileId,
+                artistProfileLineupId,
+                releaseDate,
+                List.copyOf(tracks));
+    }
+
+}
---
+++ seletivo-seplag-api/src/test/java/br/com/tcloss/seletivoseplagapi/domain/model/album/AlbumTest.java
@@ -0,0 +1,59 @@
+package br.com.tcloss.seletivoseplagapi.domain.model.album;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class AlbumTest {
+
+    @Test
+    @DisplayName("Deve criar um álbum válido")
+    public void shouldCreateValidAlbum() {
+        final var track = Mockito.mock(Track.class);
+        final var album = Album.createNew(
+                "Nome do Álbum",
+                UUID.fromString("019bf7d3-1acb-71a4-96ab-978fd3045efb"),
+                UUID.fromString("019bf7eb-b2f8-76a2-88ae-7372731b00d0"),
+                LocalDate.of(2000, 10, 01),
+                List.of(track));
+        assertThat(album).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Não deve criar um album sem faixa")
+    public void shouldNotCreateAlbumWithoutTracks() {
+        assertThatThrownBy(() -> {
+            Album.createNew(
+                    "Nome do Álbum",
+                    null,
+                    null,
+                    LocalDate.of(2000, 10, 01),
+                    List.of());
+        }).hasMessageContaining("O album deve conter ao menos uma faixa.");
+
+    }
+
+    @Test
+    @DisplayName("Não deve criar um album sem artista e sem a formação")
+    public void shouldNotCreateAlbumWithoutArtistAndLineup() {
+        final var track = Mockito.mock(Track.class);
+        assertThatThrownBy(() -> {
+            Album.createNew(
+                    "Nome do Álbum",
+                    null,
+                    null,
+                    LocalDate.of(2000, 10, 01),
+                    List.of(track));
+        }).hasMessageContaining("O ID do perfil do artista é obrigatório.")
+          .hasMessageContaining("O ID da formação do perfil do artista é obrigatório.");
+    }
+
+    
+}
