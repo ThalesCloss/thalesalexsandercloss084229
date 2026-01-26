59be82991e6c4f87664baa459f4080adb882632a
tcloss <thales.a.closs@gmail.com>
2026-01-26 19:16:47 -0400

    feat(application): controller para manipulacao de Person

---
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/api/PersonController.java
@@ -0,0 +1,28 @@
+package br.com.tcloss.seletivoseplagapi.infra.api;
+
+
+import br.com.tcloss.seletivoseplagapi.application.commandHandlers.CreatePersonCommandHandler;
+import br.com.tcloss.seletivoseplagapi.application.commands.CreatePersonCommand;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.AllArgsConstructor;
+
+@Path("/persons")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@AllArgsConstructor
+public class PersonController {
+    private final CreatePersonCommandHandler createPersonCommandHandler;
+
+    @POST
+    public Response create(@Valid CreatePersonCommand commandRequest) {
+        createPersonCommandHandler.execute(commandRequest);
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+}
