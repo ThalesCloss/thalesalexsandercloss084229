6e40ab50069598b83f03ee45dce7943e36a8fe36
tcloss <thales.a.closs@gmail.com>
2026-01-27 18:15:27 -0400

    refactor(domain): adicionado mapeamento JPA para Composition

--- seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/domain/model/composition/Composition.java
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/domain/model/composition/Composition.java
@@ -1,12 +1,22 @@
 package br.com.tcloss.seletivoseplagapi.domain.model.composition;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import br.com.tcloss.seletivoseplagapi.domain.shared.AggregateRoot;
 import br.com.tcloss.seletivoseplagapi.domain.shared.validation.Notification;
 import br.com.tcloss.seletivoseplagapi.domain.shared.vo.ProperName;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,17 +25,35 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity(name = "compositions")
 public class Composition extends AggregateRoot<UUID> {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
 
+    @Embedded
+    @AttributeOverride(
+        name = "name",
+        column = @Column(name = "title", nullable = false, length = 200)
+    )
     private ProperName title;
 
+    @Embedded
+    @AttributeOverride(
+        name = "value",
+        column = @Column(name = "iswc", nullable = false, length = 15)
+    )
     private ISWC iswc;
 
-    private List<UUID> authors;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "composition_authors",joinColumns = @JoinColumn(name = "composition_id"))
+    @Column(name = "author_id", nullable = false)
+    private List<UUID> authors = new ArrayList<>();
 
+    @Column(columnDefinition = "TEXT")
     private String lyrics;
 
+    @Column(name = "release_date")
     private LocalDate releaseDate;
 
     public static Composition createNew(String title, String iswc, List<UUID> authors, String lyrics,
@@ -33,13 +61,13 @@ public class Composition extends AggregateRoot<UUID> {
         final var notification = Notification.create();
         final var iswcValue = notification.tryExecute(() -> iswc != null ? new ISWC(iswc) : null);
         final var properTitle = notification.tryExecute(() -> new ProperName(title));
-        notification.validate(authors!=null && !authors.isEmpty(), "Deve haver ao menos um autor para a composição.");
+        notification.validate(authors != null && !authors.isEmpty(), "Deve haver ao menos um autor para a composição.");
         notification.throwIfHasErrors();
         return new Composition(
                 UUID.randomUUID(),
                 properTitle,
                 iswcValue,
-                List.copyOf(authors),
+                new ArrayList<>(authors),
                 lyrics,
                 releaseDate);
     }
--- seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/domain/model/composition/ISWC.java
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/domain/model/composition/ISWC.java
@@ -3,7 +3,9 @@ package br.com.tcloss.seletivoseplagapi.domain.model.composition;
 import java.util.regex.Pattern;
 
 import br.com.tcloss.seletivoseplagapi.domain.shared.ValueObject;
+import jakarta.persistence.Embeddable;
 
+@Embeddable
 public record ISWC(String value) implements ValueObject {
     private static final Pattern ISWC_REGEX = Pattern.compile("^T-\\d{3}\\.\\d{3}\\.\\d{3}-\\w$");
     public ISWC {
