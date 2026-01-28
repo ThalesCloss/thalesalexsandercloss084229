ace7d73ebd2b92866ebbfa6a82d2af792a54777f
tcloss <thales.a.closs@gmail.com>
2026-01-28 17:58:25 -0400

    refactor(application) documentacao dos objetos de entrada do ArtistProfile

--- seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/application/dtos/input/artistprofile/LineupDto.java
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/application/dtos/input/artistprofile/LineupDto.java
@@ -3,6 +3,8 @@ package br.com.tcloss.seletivoseplagapi.application.dtos.input.artistprofile;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -10,15 +12,19 @@ import jakarta.validation.constraints.NotNull;
 
 public record LineupDto(
     @NotBlank(message = "O nome da formação deve ser informado")
+    @Schema(description = "Rótulo que identifica a formação", examples = {"Formação original", "Segunda formação"})
     String label,
 
     @NotNull(message = "A data de inicio da formação é obrigatória")
+    @Schema(description = "Data em que a formação foi estabelecida")
     LocalDate startDate,
 
+    @Schema(description = "Para formações já encerradas, a data em que ocorreu o encerramento", required = false)
     LocalDate endDate,
 
     @NotEmpty(message = "Informe os membros da formação")
     @Valid
+    @Schema(description = "Membros presentes na formação")
     List<MemberDto> members
 ) {
 
--- seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/application/dtos/input/artistprofile/MemberDto.java
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/application/dtos/input/artistprofile/MemberDto.java
@@ -2,12 +2,17 @@ package br.com.tcloss.seletivoseplagapi.application.dtos.input.artistprofile;
 
 import java.util.UUID;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import jakarta.validation.constraints.NotNull;
 
 public record MemberDto(
     @NotNull(message = "O ID da pessoa é obrigatório")
+    @Schema(description = "Identificador da pessoa presente na formação")
     UUID personId,
+    @Schema(description = "Indica se é um artista principal")
     boolean isMainArtist,
+    @Schema(description = "Indica se é um fundador")
     boolean isFounder
 ) {
 
