e379ae2311a88eae0ff5d9d3c8f914724cda1d1a
tcloss <thales.a.closs@gmail.com>
2026-01-27 23:03:25 -0400

    feat(domain) repository para manipualcao de ArtistProfile

---
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/domain/model/artistprofile/ArtistProfileRepository.java
@@ -0,0 +1,5 @@
+package br.com.tcloss.seletivoseplagapi.domain.model.artistprofile;
+
+public interface ArtistProfileRepository {
+    void save(ArtistProfile artistProfile);
+}
---
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/persistence/repository/panache/ArtistProfileRepositoryPanache.java
@@ -0,0 +1,19 @@
+package br.com.tcloss.seletivoseplagapi.infra.persistence.repository.panache;
+
+import java.util.UUID;
+
+import br.com.tcloss.seletivoseplagapi.domain.model.artistprofile.ArtistProfile;
+import br.com.tcloss.seletivoseplagapi.domain.model.artistprofile.ArtistProfileRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+
+
+@ApplicationScoped
+public class ArtistProfileRepositoryPanache implements ArtistProfileRepository, PanacheRepositoryBase<ArtistProfile, UUID>{
+
+    @Override
+    public void save(ArtistProfile artistProfile) {
+        persist(artistProfile);
+    }
+
+}
