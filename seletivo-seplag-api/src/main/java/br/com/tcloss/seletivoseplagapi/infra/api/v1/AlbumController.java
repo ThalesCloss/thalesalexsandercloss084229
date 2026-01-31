ec894d277b2e2b31f9041160c89b4d56c577cf66
tcloss <thales.a.closs@gmail.com>
2026-01-31 10:03:28 -0400

    refactor(api): execucao do commandHandler

--- seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/api/v1/AlbumController.java
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/api/v1/AlbumController.java
@@ -1,5 +1,7 @@
 package br.com.tcloss.seletivoseplagapi.infra.api.v1;
 
+import br.com.tcloss.seletivoseplagapi.application.commandHandlers.CreateAlbumCommandHandler;
+import br.com.tcloss.seletivoseplagapi.application.commands.CreateAlbumCommand;
 import br.com.tcloss.seletivoseplagapi.application.dtos.input.album.AlbumDto;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
@@ -8,15 +10,20 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import lombok.AllArgsConstructor;
 
 @Path("/V1/albums")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@AllArgsConstructor
 public class AlbumController {
+
+    private final CreateAlbumCommandHandler createAlbumCommandHandler;
 
     @POST
     public Response criar(@Valid AlbumDto album) {
+        createAlbumCommandHandler.execute(new CreateAlbumCommand(album));
         return Response.ok().build();
-        
+
     }
 }
