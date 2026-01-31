8008eab2c65fdacd02f6bf91e49aabda2e6b3f37
tcloss <thales.a.closs@gmail.com>
2026-01-31 17:23:23 -0400

    feat(api): tratamento de recursos nao encontrados 404

--- seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/api/exception/GlobalExceptionMapper.java
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/api/exception/GlobalExceptionMapper.java
@@ -2,6 +2,7 @@ package br.com.tcloss.seletivoseplagapi.infra.api.exception;
 
 import java.time.LocalDateTime;
 
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
@@ -13,6 +14,16 @@ public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
 
     @Override
     public Response toResponse(Throwable exception) {
+        if (exception instanceof NotFoundException) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new ApiError(
+                            Response.Status.NOT_FOUND.getStatusCode(),
+                            "Não encontrado",
+                            "O recurso solicitado não foi encontrado, verifique se a URL está correta",
+                            LocalDateTime.now(),
+                            null))
+                    .build();
+        }
         Log.infof("Erro interno %s %s", Span.current().getSpanContext().getTraceId(), exception.getMessage());
         return Response
                 .status(Response.Status.INTERNAL_SERVER_ERROR)
