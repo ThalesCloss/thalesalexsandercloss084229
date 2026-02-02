43a7b8285b7203e355b558f3eced1932c4a3b8ee
tcloss <thales.a.closs@gmail.com>
2026-02-01 22:03:27 -0400

    feat(application): interface para gerenciamento de arquivos

---
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/application/ports/FileManager.java
@@ -0,0 +1,8 @@
+package br.com.tcloss.seletivoseplagapi.application.ports;
+
+import java.nio.file.Path;
+
+
+public interface FileManager {
+    public String upload(Path fileToUpload, String originalName, String contentType);
+}
