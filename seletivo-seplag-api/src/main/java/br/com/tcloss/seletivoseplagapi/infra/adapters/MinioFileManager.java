d279eb6385461438cab418d552510aa950681239
tcloss <thales.a.closs@gmail.com>
2026-02-01 22:16:03 -0400

    feat(infra): implementacao da remocao de arquivos no minio

--- seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/adapters/MinioFileManager.java
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/adapters/MinioFileManager.java
@@ -9,7 +9,9 @@ import com.github.f4b6a3.uuid.UuidCreator;
 
 import br.com.tcloss.seletivoseplagapi.application.ports.FileManager;
 import io.minio.MinioClient;
+import io.minio.RemoveObjectArgs;
 import io.minio.UploadObjectArgs;
+import io.minio.messages.DeleteObject;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -43,6 +45,16 @@ public class MinioFileManager implements FileManager {
             throw new InternalError();
         }
         return key;
+    }
+
+    @Override
+    public void delete(String fileName) {
+        try {
+            minioClient.removeObject(RemoveObjectArgs.builder().bucket(bucket).object(fileName).build());
+        } catch (Exception e) {
+            Log.errorf(e, "Erro ao excluir o arquivo %s", fileName);
+            throw new InternalError();
+        }
     }
 
 }
