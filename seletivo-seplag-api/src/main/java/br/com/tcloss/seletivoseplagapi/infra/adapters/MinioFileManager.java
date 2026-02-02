5bed51261d0a32a1fd94faa9d60a84889f941e2d
tcloss <thales.a.closs@gmail.com>
2026-02-01 22:06:13 -0400

    feat(infra): implementacao de upload com minio

---
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/adapters/MinioFileManager.java
@@ -0,0 +1,48 @@
+package br.com.tcloss.seletivoseplagapi.infra.adapters;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+
+import br.com.tcloss.seletivoseplagapi.application.ports.FileManager;
+import io.minio.MinioClient;
+import io.minio.UploadObjectArgs;
+import io.quarkus.logging.Log;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MinioFileManager implements FileManager {
+
+    private final MinioClient minioClient;
+    private final String bucket;
+
+    public MinioFileManager(MinioClient minioClient,
+            @ConfigProperty(name = "application.uploads.bucket") String bucket) {
+        this.minioClient = minioClient;
+        this.bucket = bucket;
+    }
+
+    @Override
+    public String upload(Path fileToUpload, String originalName, String contentType) {
+        final String extension = originalName.substring(originalName.lastIndexOf("."));
+        final String key = String.format("albums/%s%s", UuidCreator.getTimeOrderedEpoch(), extension);
+        try {
+            final UploadObjectArgs uploadObject = UploadObjectArgs.builder()
+                    .bucket(bucket)
+                    .object(key)
+                    .contentType(contentType)
+                    .filename(fileToUpload.toString())
+                    .build();
+            minioClient.uploadObject(uploadObject);
+
+        } catch (Exception e) {
+            Log.errorf(e, "Erro no upload do arquivo %s", key);
+            throw new InternalError();
+        }
+        return key;
+    }
+
+}
