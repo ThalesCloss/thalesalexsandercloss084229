ecd2e4fc882838c6f5e664798e3741dff666ed51
tcloss <thales.a.closs@gmail.com>
2026-02-01 22:01:57 -0400

    test(e2e): configurar testcontainer minio

--- seletivo-seplag-api/src/test/java/br/com/tcloss/seletivoseplagapi/infra/TestContainerResource.java
+++ seletivo-seplag-api/src/test/java/br/com/tcloss/seletivoseplagapi/infra/TestContainerResource.java
@@ -3,6 +3,7 @@ package br.com.tcloss.seletivoseplagapi.infra;
 import java.util.Map;
 
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -16,6 +17,12 @@ public class TestContainerResource implements QuarkusTestResourceLifecycleManage
 
     private static final String DB_NAME = "teste-bd";
 
+    private static final String MINIO_USER = "miniouser";
+
+    private static final String MINIO_PASSWORD = "miniopass";
+
+    private static final String MINIO_BUCKET = "uploads";
+
     static PostgreSQLContainer<?> db = new PostgreSQLContainer<>("postgres:15-alpine")
             .withDatabaseName(DB_NAME)
             .withUsername(DB_USER)
@@ -23,13 +30,20 @@ public class TestContainerResource implements QuarkusTestResourceLifecycleManage
 
     static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:alpine"))
             .withExposedPorts(6379);
+    static MinIOContainer minio = new MinIOContainer("minio/minio:RELEASE.2025-09-07T16-13-09Z")
+            .withUserName(MINIO_USER)
+            .withPassword(MINIO_PASSWORD);
 
     @Override
     public Map<String, String> start() {
         db.start();
         redis.start();
-
+        minio.start();
         return Map.of(
+                "quarkus.minio.access-key", minio.getUserName(),
+                "quarkus.minio.secret-key", minio.getPassword(),
+                "quarkus.minio.url", minio.getS3URL(),
+                "application.uploads.bucket", MINIO_BUCKET,
                 "quarkus.datasource.jdbc.url", db.getJdbcUrl(),
                 "quarkus.datasource.username", DB_USER,
                 "quarkus.datasource.password", DB_PASSWORD,
@@ -40,6 +54,7 @@ public class TestContainerResource implements QuarkusTestResourceLifecycleManage
     public void stop() {
         redis.stop();
         db.stop();
+        minio.stop();
     }
 
 }
