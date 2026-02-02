bf99e34b82a4df84a1e4428f58be00648e33c07d
tcloss <thales.a.closs@gmail.com>
2026-02-01 22:12:25 -0400

    feat(application): definicao da remocao de arquivos

--- seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/application/ports/FileManager.java
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/application/ports/FileManager.java
@@ -2,7 +2,8 @@ package br.com.tcloss.seletivoseplagapi.application.ports;
 
 import java.nio.file.Path;
 
-
 public interface FileManager {
     public String upload(Path fileToUpload, String originalName, String contentType);
+
+    public void delete(String fileName);
 }
