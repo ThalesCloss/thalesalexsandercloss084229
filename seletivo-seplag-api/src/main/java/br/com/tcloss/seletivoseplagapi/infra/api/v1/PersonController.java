6799690a4df69dd160001ead311cce04e7603cd8
tcloss <thales.a.closs@gmail.com>
2026-02-03 18:30:55 -0400

    feat(api) consulta de pessoas

--- seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/api/v1/PersonController.java
+++ seletivo-seplag-api/src/main/java/br/com/tcloss/seletivoseplagapi/infra/api/v1/PersonController.java
@@ -1,12 +1,20 @@
 package br.com.tcloss.seletivoseplagapi.infra.api.v1;
 
-
 import br.com.tcloss.seletivoseplagapi.application.commandHandlers.CreatePersonCommandHandler;
 import br.com.tcloss.seletivoseplagapi.application.commands.CreatePersonCommand;
+import br.com.tcloss.seletivoseplagapi.application.dtos.input.OrderInputDto;
+import br.com.tcloss.seletivoseplagapi.application.dtos.input.PaginationInputDto;
+import br.com.tcloss.seletivoseplagapi.application.dtos.input.PersonSearchDto;
+import br.com.tcloss.seletivoseplagapi.application.dtos.output.MultipleItemsResult;
+import br.com.tcloss.seletivoseplagapi.application.dtos.output.PersonResponse;
+import br.com.tcloss.seletivoseplagapi.application.queries.SearchPersonsQuery;
+import br.com.tcloss.seletivoseplagapi.application.queryHandlers.SearchPersonsQueryHandler;
 import br.com.tcloss.seletivoseplagapi.infra.api.annotation.RateLimit;
 import io.quarkus.security.Authenticated;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -22,11 +30,21 @@ import lombok.AllArgsConstructor;
 @RateLimit
 public class PersonController {
     private final CreatePersonCommandHandler createPersonCommandHandler;
+    private final SearchPersonsQueryHandler searchPersonsQueryHandler;
 
     @POST
     public Response create(@Valid CreatePersonCommand commandRequest) {
         createPersonCommandHandler.execute(commandRequest);
         return Response.status(Response.Status.CREATED).build();
+    }
+
+    @GET
+    public MultipleItemsResult<PersonResponse> search(
+            @BeanParam PersonSearchDto personSearchDto,
+            @BeanParam PaginationInputDto paginationInputDto,
+            @BeanParam OrderInputDto orderInputDto) {
+        return searchPersonsQueryHandler.query(new SearchPersonsQuery(
+                personSearchDto, paginationInputDto, orderInputDto));
     }
 
 }
